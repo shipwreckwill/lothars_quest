5c7adbc7b5b306b46ca239140e2333d091109a6b
Will <shipwreckwill@gmail.com>
2020-11-09 23:52:18 -0800

    Random Dice Nmber

--- resources.py
+++ resources.py
@@ -42,7 +42,7 @@ class DiceResource:
                     '\n --D04\n\n')
     
     def on_post(self, req, resp):
-        roll = random(int(req.get_param("die", required=True)))
+        roll = random.randrange(int(req.get_param("die", required=True)))
         resp.status = falcon.HTTP_200
         message = {'roll': roll}
         resp.body = json.dumps(message)
